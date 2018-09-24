969f8b732c08710217bd331a1b2bb09c91873d40
Jacob Marks <jacobjmarks@outlook.com>
2018-09-24 13:51:49 +1000

    Setup basic limb mesh drawing

--- Assets/Scripts/Limb.cs
+++ Assets/Scripts/Limb.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Limb : MonoBehaviour {
 
+    [Header("Mesh")]
     public float width;
     public float height;
+    public Material material;
+    public Color color;
+
+    [Header("Joints")]
+    public Vector2 selfJoint;
+    public Vector2 childJoint;
 
     private void Awake() {
         DrawMesh();
     }
 
     private void DrawMesh() {
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        Mesh mesh = gameObject.AddComponent<MeshFilter>().mesh;
+        mesh.Clear();
 
+        mesh.vertices = new Vector3[] {
+            new Vector3(0, 0),
+            new Vector3(0, height),
+            new Vector3(width, height),
+            new Vector3(width, 0)
+        };
+
+        mesh.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
+        mesh.colors = new Color[] { color, color, color, color };
+
+        mesh.RecalculateBounds();
     }
 
 }
