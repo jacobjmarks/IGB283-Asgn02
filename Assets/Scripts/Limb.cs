debb815f6060c536658d82fa477ccf382f305b31
Jacob Marks <jacobjmarks@outlook.com>
2018-09-24 13:38:36 +1000

    Initial commit

---
+++ Assets/Scripts/Limb.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limb : MonoBehaviour {
+
+    public float width;
+    public float height;
+
+    private void Awake() {
+        DrawMesh();
+    }
+
+    private void DrawMesh() {
+
+    }
+
+}
