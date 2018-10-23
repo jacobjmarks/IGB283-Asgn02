9eb9e82aac7aef03984477c07ab28ee867e0d37b
Jacob Marks <jacobjmarks@outlook.com>
2018-10-23 22:42:19 +1000

    Added Limb wobble script
    Used to add bot head nod

---
+++ Assets/Scripts/LimbWobble.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimbWobble : MonoBehaviour {
+
+    public float angle;
+    public float speed;
+
+    private Limb limb;
+    private float startAngle;
+
+    private void Start() {
+        limb = GetComponent<Limb>();
+        startAngle = transform.rotation.eulerAngles.z;
+    }
+
+    private void Update() {
+        float newAngle = startAngle + Mathf.Sin(speed * Time.realtimeSinceStartup) * angle/2;
+        limb.Rotate(newAngle - transform.eulerAngles.z);
+    }
+
+}
